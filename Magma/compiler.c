a979369457bfcb61ece80c52a65f25578a2bb847
r4dianc3 <jai-webster@outlook.com>
2026-03-17 14:20:33 +0530

    ADD includes and Init func

--- Magma/compiler.c
+++ Magma/compiler.c
@@ -1,11 +1,17 @@
 #include "Lexer/lexer.c"
+#include "Parser/parser.c"
+#include "semantic/symbol_table.c"
+#include "semantic/type_checker.c"
+#include "ast/ast.c"
 
-int main()
-{
+void init() {
     void lexer_init();
     void tokenstream_init();
     void tokenstream_free();
+}
 
+int main()
+{
     void lexer_scan_all();
 
     return 0;
